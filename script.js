af8f4081d673b1e5a6130879918b47efdc0df064
ivan91111949 <56313735+ivan91111949@users.noreply.github.com>
2019-11-19 20:37:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-var request = new XMLHttpRequest
-request.onreadystatechange = function (event) {
-    this.readyState === 4 ? this.status === 200 ?
-        console.log (this.responseText) :
-            console.warn ('request error') :
-                null
+function getData (url,callback) {
+  var request = Object.assign(
+    new XMLHttpRequest, {
+      onreadystatechange :function (event) {
+        this.readyState === 4 ? this.status === 200 ?
+          callback(this.responseText) :
+             console.warn('error') : null
+      }
+    }
+  )
+  request.open("GET",url)
+  request.send()
 }
-request.open('GET', '/user.json')
-request.send()
+
+function showMarkup (html) {
+    document.body.innerHTML += html
+}
+
+function showText (text) {
+    document.body.innerHTML += `<h3> ${text}</h3>`
+}
+
+
+var showGallery = function (response) {
+  pictures = JSON.parse(response)
+  pictures.forEach(
+        picture => document.body.appendChild(document.cerateElement('img')).src = picture.ref
+    )
+}
+
+getData("./user2.json",showGallery)
+getData('hello.txt', showText)
+getData('hello.html',showMarkup)
